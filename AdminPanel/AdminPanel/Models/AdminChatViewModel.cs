7c393a943c6755ba2272c5a6c0fa33e42d8782ce
z <yucezisan@gmail.com>
2026-03-06 13:57:31 +0300

    chore: remove source folders after migrating to independent repositories

--- AdminPanel/AdminPanel/Models/AdminChatViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdminPanel.Models
-{
-    public class AdminChatViewModel
-    {
-        public MusteriViewModel Musteri { get; set; }
-        public List<MesajViewModel> Mesajlar { get; set; }
-    }
-}
